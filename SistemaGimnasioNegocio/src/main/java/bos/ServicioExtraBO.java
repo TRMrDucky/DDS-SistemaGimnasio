907cc9c77754ad75bedda4524d68e9093365b7f4
janethcrgq <janethcrgq02@gmail.com>
2025-04-15 13:03:43 -0700

    pantalla principal

--- SistemaGimnasioNegocio/src/main/java/bos/ServicioExtraBO.java
+++ SistemaGimnasioNegocio/src/main/java/bos/ServicioExtraBO.java
@@ -21,7 +21,10 @@ public class ServicioExtraBO implements IServicioExtraBO{
     public ServicioExtraBO(IServicioExtraDAO servicioDAO) {
         this.servicioDAO = servicioDAO;
     }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     public List<ServicioExtraDTO> obtenerServiciosExtrasDTO() {
         return ServicioExtraMapper.toListDTO(servicioDAO.obtenerServiciosExtrasDTO());
     }
