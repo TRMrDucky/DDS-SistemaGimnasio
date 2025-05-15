e484ea998a3578224215df7e8d9272ef71fbc462
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-05-15 16:19:57 -0700

    me falto este dispose

--- SistemaGimnasioPresentacion/src/main/java/presentacion/SeleccionOpcionServicioExtra.java
+++ SistemaGimnasioPresentacion/src/main/java/presentacion/SeleccionOpcionServicioExtra.java
@@ -42,7 +42,7 @@ public class SeleccionOpcionServicioExtra extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setBackground(new java.awt.Color(48, 150, 244));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
