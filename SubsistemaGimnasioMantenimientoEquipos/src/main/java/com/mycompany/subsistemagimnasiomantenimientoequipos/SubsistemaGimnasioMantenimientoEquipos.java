d28652971554f6b74ce1f35d970b2664006188bb
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-05-15 12:05:41 -0700

    se agrego subsistema

---
+++ SubsistemaGimnasioMantenimientoEquipos/src/main/java/com/mycompany/subsistemagimnasiomantenimientoequipos/SubsistemaGimnasioMantenimientoEquipos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.subsistemagimnasiomantenimientoequipos;
+
+/**
+ *
+ * @author Cricri
+ */
+public class SubsistemaGimnasioMantenimientoEquipos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
