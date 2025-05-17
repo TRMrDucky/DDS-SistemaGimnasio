49829c9b1da198457fcdd4bd1e6880cc258c446a
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-05-16 22:53:26 -0700

    esqueleto de subsistema

---
+++ SubsistemaGimnasioMantenimientoEquipos/src/main/java/excepciones/SubsistemaMantenimientoEquiposException.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author Cricri
+ */
+public class SubsistemaMantenimientoEquiposException extends Exception {
+
+    public SubsistemaMantenimientoEquiposException() {
+    }
+    
+
+    public SubsistemaMantenimientoEquiposException(String message) {
+        super(message);
+    }
+
+    public SubsistemaMantenimientoEquiposException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+    
+}
