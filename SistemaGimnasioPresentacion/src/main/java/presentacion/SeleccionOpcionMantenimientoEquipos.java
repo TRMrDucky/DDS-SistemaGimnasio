0ec4564105a3e9bb88cea2c49475613adc0fcf7d
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-05-20 21:51:40 -0700

    flujo de pantallas

--- SistemaGimnasioPresentacion/src/main/java/presentacion/SeleccionOpcionMantenimientoEquipos.java
+++ SistemaGimnasioPresentacion/src/main/java/presentacion/SeleccionOpcionMantenimientoEquipos.java
@@ -129,17 +129,17 @@ public class SeleccionOpcionMantenimientoEquipos extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-            this.setVisible(false);
-    SwingUtilities.invokeLater(() -> {
+            
         control.openFormPantallaBuscadorEquipos(ModoUso.REGISTRO_MANTENIMIENTO);
-});
+         this.setVisible(false);
+
 
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
       
         control.openFormPantallaBuscadorEquipos(ModoUso.CONSULTA_HISTORIAL);
-         this.setVisible(false);
+        this.setVisible(false);
   
     }//GEN-LAST:event_jButton3ActionPerformed
 
